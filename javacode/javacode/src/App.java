7826c0ef4579b0312e2f808b8a47698f499e9356
Adilson Agottani dos Reis <adilson.reis@pucpr.edu.br>
2020-12-16 11:25:38 -0300

    Exercicio sleep in adicionado

--- javacode/javacode/src/App.java
+++ javacode/javacode/src/App.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Scanner teclado = new Scanner(System.in);
+        javaclass aux= new javaclass();
+
+        System.out.println("Will we sleep in?");
+        System.out.println("Is it a weekday? Type yes or no:");
+        String weekday= teclado.nextLine();
+        boolean wd=false;
+        System.out.println("Are we on vacation? Type yes or no:");
+        String vacation= teclado.nextLine();
         
+        boolean vct=false;
+
+        if (weekday.equals("yes")){
+            
+            wd=true;
+        }
+
+        if (vacation.equals("yes")){
+            
+            vct=true;
+        }
+        
+        System.out.println(aux.sleepIn(wd, vct));
+        teclado.close();
     }
 }
